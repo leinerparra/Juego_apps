89b2f4c0fd245a9fe4100b901cb5127feec7f1e5
Serna28 <73318865+leinerparra@users.noreply.github.com>
2022-11-07 18:57:15 -0500

    Añadidas mas funcionaldiades de quiz

--- app/src/main/java/com/example/trivia/JuegoTrivia.java
+++ app/src/main/java/com/example/trivia/JuegoTrivia.java
@@ -1,14 +1,33 @@
 package com.example.trivia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class JuegoTrivia extends AppCompatActivity {
+
+    private TextView questions;
+    private TextView question;
+    private AppCompatButton opcion1,opcion2,opcion3,opcion4;
+    private Timer TiempoTrivia;
+    private AppCompatButton siguienteBtn;
+
+    private int TiempoTotal =1;
+    private int seconds =0;
+
+    private final List<ListaPreguntas> questionLists = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +38,54 @@ public class JuegoTrivia extends AppCompatActivity {
         final TextView timer = findViewById(R.id.timer);
         final TextView selectedTopicName = findViewById(R.id.TopicName);
 
+        questions = findViewById(R.id.Preguntas);
+        question = findViewById(R.id.Pregunta);
+
+        opcion1 = findViewById(R.id.Opcion1);
+        opcion2 = findViewById(R.id.Opcion2);
+        opcion3 = findViewById(R.id.Opcion3);
+        opcion4 = findViewById(R.id.Opcion4);
+
+        siguienteBtn = findViewById(R.id.SiguienteBTN);
+
         final String getSelectedName = getIntent().getStringExtra("SelectedTopic");
 
         selectedTopicName.setText(getSelectedName);
+
+
+
+    }
+
+    private void startTimer(TextView timerTextView)
+    {
+        TiempoTrivia = new Timer();
+
+        TiempoTrivia.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+
+                if(seconds == 0)
+                {
+                    TiempoTotal--;
+                    seconds = 50;
+                }
+                else if(seconds == 0 && TiempoTotal ==0)
+                {
+                    TiempoTrivia.purge();
+                    TiempoTrivia.cancel();
+
+                    Toast.makeText(JuegoTrivia.this,"Tiempo FInal", Toast.LENGTH_SHORT).show();
+
+                    Intent intento = new Intent(JuegoTrivia.this, Resultados.class);
+                    intento.putExtra("Correcto", );
+
+                }
+            }
+        }, 1000,1000);
+    }
+
+    private int getRespuestasCorrectas()
+    {
 
     }
 }
