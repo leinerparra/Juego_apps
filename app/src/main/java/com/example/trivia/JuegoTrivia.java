140e7e11d9c6a8775fecd6b67e11879089b01105
Serna28 <73318865+leinerparra@users.noreply.github.com>
2022-11-07 18:27:52 -0500

    Base Cuestionario

--- app/src/main/java/com/example/trivia/JuegoTrivia.java
+++ app/src/main/java/com/example/trivia/JuegoTrivia.java
@@ -3,6 +3,10 @@ package com.example.trivia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class JuegoTrivia extends AppCompatActivity {
 
@@ -11,6 +15,13 @@ public class JuegoTrivia extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego_trivia);
 
+        final ImageView backBTn = findViewById(R.id.backBtn);
+        final TextView timer = findViewById(R.id.timer);
+        final TextView selectedTopicName = findViewById(R.id.TopicName);
+
         final String getSelectedName = getIntent().getStringExtra("SelectedTopic");
+
+        selectedTopicName.setText(getSelectedName);
+
     }
 }
